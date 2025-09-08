7f0735d7c7f180190bc013dbdc5844aa750c0d47
Pratiush Paliwal <pratiushpaliwal.office@gmail.com>
2025-09-08 15:06:41 +0530

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -2,7 +2,7 @@
 Write a program to calculate simple and compound interest for given principal, rate, and time.
 */
 #include <stdio.h>
-#include <math.h>  // Required for pow() function
+#include <math.h> 
 
 int main() {
     float principal, rate, time;
